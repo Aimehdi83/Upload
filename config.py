d8a32931f6f05b890f8c127601bf053491ba2b30
Aimehdi83 <ai21101383@gmail.com>
2025-05-11 15:58:02 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 BOT_TOKEN = "7086274656:AAFBto5kR50yXUQHolID0sv9GGnrzy3YpvM"
-WEBHOOK_URL = "https://tttttt-v1kw.onrender.com/webhook"
+WEBHOOK_URL = "https://upload-1-iwpg.onrender.com/webhook"
 ADMIN_IDS = [6387942633, 7189616405, 5459406429]  # شناسه عددی ادمین‌ها
 CHANNEL_TAG = "🔥@hottof | تُفِ داغ " 
 PING_INTERVAL = 240  # فاصله زمانی بین پینگ‌ها به ثانیه
